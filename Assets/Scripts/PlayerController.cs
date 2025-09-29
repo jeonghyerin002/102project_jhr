5d3c016ca1a5de145bec7e2cf8a4d8a5ea4d3a5c
jeinghyerin002 <jhr102446@gmail.com>
2025-09-29 15:33:04 +0900

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,6 +38,8 @@ public class PlayerController : MonoBehaviour
     private bool wasGrounded;                             //이전 프레임에 땅이였는지
     private float attackTimer;
 
+    private bool isUIMode = false;                    //UI모드 설정
+
 
     void Start()
     {
@@ -47,12 +49,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        CheckGrounded();
-        HandleLanding();
-        HandleMovement();
-        HandleJump();
-        HandleAttack();
-        UpdateAnimator();
+
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleCursorLock();
+        }
+
+        if(!isUIMode)
+        {
+            CheckGrounded();
+            HandleLanding();
+            HandleMovement();
+            HandleJump();
+            HandleAttack();
+            UpdateAnimator();
+        }
     }
 
     void CheckGrounded()
@@ -190,5 +201,32 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("isFalling", isFalling);
         animator.SetBool("isLanding", isLanding);
 
+    }
+
+    public void SetCursorLock(bool lockCursor)
+    {
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            isUIMode = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            isUIMode = true;
+        }
+    }
+
+    public void ToggleCursorLock()
+    {
+        bool shouldLock = Cursor.lockState != CursorLockMode.Locked;
+        SetCursorLock(shouldLock);
+    }
+
+    public void SetUIMode(bool uiMode)
+    {
+        SetCursorLock(!uiMode);
     }
 }
